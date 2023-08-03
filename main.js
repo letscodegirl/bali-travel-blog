c5c9b68848420c53037515792713b282ff0a706a
letscodegirl <116798082+letscodegirl@users.noreply.github.com>
2023-08-03 18:44:38 +0900

    changes

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log(`First one to find this get bonus points. extra if you can tell the 
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 3
+  slidesToScroll: 1
 });
 
 
