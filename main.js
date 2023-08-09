7f4a9a96c68875d5da488470de02c04bae9b21bd
letscodegirl <116798082+letscodegirl@users.noreply.github.com>
2023-08-10 08:23:31 +0900

    md

--- main.js
+++ main.js
@@ -4,8 +4,28 @@ console.log(`First one to find this get bonus points. extra if you can tell the 
 $('.multiple-items').slick({
   infinite: true,
   slidesToShow: 3,
-  slidesToScroll: 1
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 3
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        slidesToShow: 1
+      }
+    }
+  ]
+
+
+
 });
-
-
 
